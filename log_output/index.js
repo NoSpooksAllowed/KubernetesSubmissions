aff42c4be0835ce4286139b982d0c88ec1ac3b76
NoSpooksAllowed <hersgori4@protonmail.com>
2025-08-01 16:20:06 -0300

    New code, now app send back random string to a user who is requested

--- log_output/index.js
+++ log_output/index.js
@@ -1,35 +1,22 @@
-const crypto = require("crypto");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const createRandomString = () => Math.random().toString(36).substr(2, 6);
 
-/**
- * Generates a cryptographically secure random hash.
- * @param {number} [length=32] - The number of random bytes to generate (determines entropy).
- * @param {string} [algorithm='sha256'] - The hashing algorithm to use (e.g., 'sha256', 'sha512').
- * @returns {string} The generated hash as a hexadecimal string.
- */
-function generateRandomHash(length = 32, algorithm = "sha256") {
-  const randomBytes = crypto.randomBytes(length);
-  const hash = crypto.createHash(algorithm);
-  hash.update(randomBytes);
+const startingString = createRandomString();
 
-  return hash.digest("hex");
-}
+app.get("/", (req, res) => {
+  if (req.path.includes("favicon.ico")) {
+    return res.end();
+  }
 
-const randomHash = generateRandomHash();
-
-const intervalId = setInterval(() => {
-  const timestamp = new Date().toISOString();
-
-  console.log(`${timestamp}: ${randomHash}`);
-}, 5000);
-
-process.on("SIGINT", () => {
-  console.log("\nReceived SIGINT. Stopping hash generation loop...");
-  clearInterval(intervalId);
-  process.exit(0);
+  const stringNow = createRandomString();
+  console.log("--------------------");
+  console.log(`Responding with ${stringNow}`);
+  res.send(`${startingString}: ${stringNow}`);
 });
 
-process.on("SIGTERM", () => {
-  console.log("\nReceived SIGTERM. Stopping hash generation loop...");
-  clearInterval(intervalId);
-  process.exit(0);
+console.log(`Started with ${startingString}`);
+app.listen(PORT, () => {
+  console.log(`Express app listening on port ${PORT}`);
 });
